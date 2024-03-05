c157a1bc4a1e3dd0926d28c522d03ec2c3a75bc3
KALEEMULLAH <160345037+Kaleemullah3095@users.noreply.github.com>
2024-03-05 23:42:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var favouriteNumber = 4;
+console.log("MyfavouriteNumber is ".concat(favouriteNumber));
---
+++ app.ts
@@ -0,0 +1,2 @@
+let favouriteNumber =4
+console.log(`MyfavouriteNumber is ${favouriteNumber}`);
